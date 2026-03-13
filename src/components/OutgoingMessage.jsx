92f8f415851b750fdb19cb3ef31527d82bf01fad
cuongchuck <cuong.buiducmanh@gmail.com>
2026-03-13 14:08:19 +0700

    Add outgoing message component

---
+++ src/components/OutgoingMessage.jsx
@@ -0,0 +1,11 @@
+const OutgoingMessage = () => {
+  return (
+    <div className="d-flex justify-content-end text-white">
+      <div className="rounded-5 out-msg py-2 px-3 msg-wrapper o overflow-visible mb-5">
+        aaaaaaaaaa aaaaaaaaa aaaaaaaaaaaa aaaa aaaa aaaaaa aaaaaaaaaa aaaaaaaaaaaaa
+      </div>
+    </div>
+  );
+};
+
+export default OutgoingMessage;
